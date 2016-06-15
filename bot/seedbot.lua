c834ef63a352b31676ad90a73124a750d1186ec5
mahdifashion <banersazfa@gmail.com>
2016-06-16 01:55:04 +0430

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -226,7 +226,7 @@ function create_config( )
     "leave_ban",
     "admin"
     },
-    sudo_users = {80882965,217704479,143723991},--Sudo users
+    sudo_users = {80882965,217704479,226789204},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v2 - Open Source
